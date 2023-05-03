4a0937e6df7d1268c0623b9207bc56f95f91b2d6
Majid Aghighi <majid.aghighy@gmail.com>
2023-05-04 00:13:32 +0330

    feat:Solve Question8

---
+++ Question 8/script.js
@@ -0,0 +1,10 @@
+var closebtns = document.getElementsByClassName("contain");
+var i;
+
+for (i = 0; i < closebtns.length; i++) {
+  closebtns[i].addEventListener("click", function() {
+    this.parentElement.style.display = 'none';
+  });
+  
+}
+
